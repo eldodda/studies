3fa40c44bed3189e5a78be44da4e5b109725e0ef
Douglas Lima <dugbits@gmail.com>
2026-01-27 18:41:17 +0000

    Início do projeto do curso de React
    - Sintaxes básicas do JSX;
    - Componentização;
    - Styled Components;
    - Estados

---
+++ projeto-react/src/componentes/input/index.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+    order: 1px solid #fff;
+    background: transparent;
+    border: 1px solid blanchedalmond;
+    padding: 20px 140px;
+    border-radius: 50px;
+    width: 200px;
+    color: blanchedalmond;
+    font-size: 16px;
+    margin-bottom: 10px;
+
+    &::placeholder{
+        color: blanchedalmond;
+        font-size: 16px;
+    }
+`
+
+export default Input
